5a83d004b0f198bddf7b3ed1af8841e0d8abc615
Sergio Flores <byoigres@gmail.com>
2019-03-10 15:25:10 -0700

    Se agrega styled-components y el MainLayout

---
+++ src/components/layouts/MainLayout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainLayout = ({ children }) => (
+    <div style={{backgroundColor: 'red'}}>
+        {children}
+    </div>
+)
+
+export default MainLayout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import React from "react"
+import MainLayout from '../components/layouts/MainLayout'
 
 const IndexPage = () => (
-  <h1>Hi people</h1>
+  <MainLayout>
+    <h1>Hi people</h1>
+  </MainLayout>
 )
 
 export default IndexPage
