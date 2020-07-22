84473a6557a83f8c889eaac4e056947ebe46231b
Sergio Flores <byoigres@gmail.com>
2020-07-22 11:40:16 -0600

    Added eleventy-plugin-sass

--- eleventy/collections/index.js
+++ eleventy/collections/index.js
@@ -1,13 +1,12 @@
 const htmlmin = require('html-minifier');
 
 function addTransforms(eleventyConfig) {
-  const env = process.env.ELEVENTY_ENV;
 
   eleventyConfig.addCollection('posts', function (collection) {
     return collection.getFilteredByGlob('./content/**/*.md').filter(function (item) {
 
       if (item.data.permalink) {
-        if (env === 'prod' && item.data.draft === true) {
+        if (process.env.ELEVENTY_ENV === 'prod' && item.data.draft === true) {
           return false
         }
 
--- eleventy/plugins/index.js
+++ eleventy/plugins/index.js
@@ -18,6 +18,17 @@ function addPlugins(eleventyConfig) {
     // The default (false) only wraps when line numbers are passed in.
     alwaysWrapLineHighlights: false
   });
+
+  /**
+   * https://www.npmjs.com/package/eleventy-plugin-sass
+   */
+  eleventyConfig.addPlugin(require("eleventy-plugin-sass"), {
+    watch: ['./src/styles/**/*.{scss,sass}', '!node_modules/**'], // The sass files you wish to compile (and watch when you serve)
+    sourcemaps: process.env.ELEVENTY_ENV === 'prod', // Add sourcemaps next to your sass files
+    cleanCSS: process.env.ELEVENTY_ENV === 'prod', // 	Runs the css trough cleanCSS
+    cleanCSSOptions: {}, // 	Options to pass to cleanCSS
+    autoprefixer: true // Adds browser specific prefixes if needed (adheres to BrowserList)
+  });
 }
 
 module.exports = addPlugins;
