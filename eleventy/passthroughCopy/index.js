915310175e6096f95ae4961acebc0544b27237ad
Sergio Flores <byoigres@gmail.com>
2020-08-08 16:10:18 -0600

    Added CNAME file to dist

--- eleventy/passthroughCopy/index.js
+++ eleventy/passthroughCopy/index.js
@@ -3,6 +3,7 @@ const globby = require('globby');
 function addPassthroughCopy(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy('CNAME');
   eleventyConfig.addPassthroughCopy({
     'node_modules/waypoints/lib/noframework.waypoints.min.js': 'js/waypoints.min.js'
   });
