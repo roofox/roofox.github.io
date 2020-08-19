5253bbc0e72db26f357b60ff1963b07eab842237
Sergio Flores <byoigres@gmail.com>
2020-08-18 22:55:03 -0600

    Cover is replaced to new style/animation

--- src/components/covers/18c7.jsx
+++ src/components/covers/18c7.jsx
@@ -1,106 +1,99 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react"
+import styled, { keyframes, css } from "styled-components"
+import { adjustHue } from "polished"
 
-const clipHeight = '50px';
-const leftOffset = 29;
+const metric = "s";
+const time = "1";
+const d = "0.08em"
+const n = 3;
+const e = "cubic-bezier(0.860, 0.000, 0.070, 1.000)";
+const front = "black"
+const back = "lightblue"
+
+const effect = keyframes`
+0%{
+  transform: translate3d(0,0,0);
+  text-shadow: 0em 0em 0 ${back};
+  color: ${front};
+}
+30%{
+  transform: translate3d(0,0,0);
+  text-shadow: 0em 0em 0 ${back};
+  color: ${front};
+}
+70%{
+  transform: translate3d(${d},-${d},0);
+  text-shadow: -${d} ${d} ${back};
+  color: ${front};
+}
+100%{
+  transform: translate3d(${d},-${d},0);
+  text-shadow: -${d} ${d} ${back};
+  color: ${front};
+}
+`;
 
 const Container = styled.div`
-    background-color: #5138BE;
-    color: #FFFFFF;
-    display: flex;
+  background: white;
 `;
 
-const Words = styled.ul`
-    margin: 0 auto;
-    padding: 80px 0;
-    font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif;
-    font-size: 68px;
-    font-weight: 900;
-    letter-spacing: -2px;
-    text-transform: uppercase;
-    // Sort out nasty text fuzz
-    transform: translate3d(0, 0, 0);
-    -webkit-font-smoothing: antialiased;
-    -webkit-font-kerning: normal;
-    -webkit-text-size-adjust: 100%;
-
-    &:hover {
-        p {
-            transform: translate3d(0, -${p => p.clipHeight}, 0);
-        }
-    }
+const Paragraph = styled.h1`
+  font-family: Futura, sans-serif;
+  font-weight: 900;
+  font-size: 80px;
+  padding: 80px;
 `;
 
-const Line = styled.li`
-    height: ${({ clipHeight }) => clipHeight};
-    overflow: hidden;
-    position: relative;
+const Letter = styled.span`
+  position: relative;
+  display: inline-block;
+  animation: ${effect} ${time}s infinite alternate ${e};
 
-    // Change the perspective of each alternating line
-    &:nth-child(odd) {
-        transform: skew(60deg, -30deg) scaleY(.66667);
+  ${[...Array(n).keys()].map((i) => 
+    css`
+      &:nth-last-child(${i + 1}n) {
+        animation-delay: -${(time * i) / n / 2}${metric};
+      }
+    `
+  )}
+  /* @for $i from 1 through $n {
+    &:nth-last-child(#{$i}n) {
+      animation-delay: -$t * $i/$n/2;
     }
-    &:nth-child(even) {
-        transform: skew(0deg, -30deg) scaleY(1.33333);
-    }
+  } */
+`
 
-    // Loop over the total lines and apply a left offset
-    // @for $i from 1 through $total-lines+1 {
-    //     &:nth-child(#{$i}) {
-    //         left: $left-offset * $i;
-    //     }
-    // }
-    ${
-        ({ lines, leftOffset }) => [...Array(lines).keys()].map(index => (
-            `
-            &:nth-child(${index + 1}}) {
-                left: ${leftOffset * (index + 1)}px;
-            }
-            `
-        ))
-    }
-`;
+const Covercc9a = ({ text }) => (
+  <Container>
+    <Paragraph>
+      <Letter>U</Letter>
+      <Letter>N</Letter>
+      <Letter>A</Letter>
+      <Letter>B</Letter>
+      <Letter>R</Letter>
+      <Letter>E</Letter>
+      <Letter>V</Letter>
+      <Letter>E</Letter>
+      <br />
+      <Letter>H</Letter>
+      <Letter>I</Letter>
+      <Letter>S</Letter>
+      <Letter>T</Letter>
+      <Letter>O</Letter>
+      <Letter>R</Letter>
+      <Letter>I</Letter>
+      <Letter>A</Letter>
+      <br />
+      <Letter>D</Letter>
+      <Letter>E</Letter>
+      <Letter>D</Letter>
+      <Letter>E</Letter>
+      <Letter>B</Letter>
+      <Letter>I</Letter>
+      <Letter>A</Letter>
+      <Letter>N</Letter>
+    </Paragraph>
+  </Container>
+)
 
-const Word = styled.p`
-    height: ${p => p.clipHeight};
-    line-height: $line-height;
-    padding: 0 10px;
-    transition: all .4s ease-in-out;
-    transform: translate3d(0, 0, 0);
-    vertical-align: top;
-    white-space: nowrap;
-    line-height: 1rem!important;
-`;
-
-const Cover18c7 = ({ text }) => (
-    <Container>
-        <Words clipHeight={clipHeight}>
-            {
-                text.split(' ').map((word, index, { length }) => {
-                    return (
-                        <Line
-                            key={`${word}-${index}`}
-                            clipHeight={clipHeight}
-                            leftOffset={leftOffset}
-                            lines={length}
-                        >
-                            <Word clipHeight={clipHeight}>
-                                {
-                                    `${index === 0 ? ' ' : `${word}`}(${index})`
-                                }
-                            </Word>
-                            <Word clipHeight={clipHeight}>
-                                {
-                                    `${(index + 1) === length ? ' ' : `${word}`}(${index})`
-                                }
-                            </Word>
-                        </Line>
-                    )
-                })
-            }
-        </Words>
-    </Container>
-);
-
-export default Cover18c7
-
+export default Covercc9a
