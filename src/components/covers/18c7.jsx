e96764b28c17a2e3d6ff3f8f05f646fa0625948c
Sergio Flores <byoigres@gmail.com>
2020-08-19 00:00:38 -0600

    Added media queries for cover

--- src/components/covers/18c7.jsx
+++ src/components/covers/18c7.jsx
@@ -1,97 +1,99 @@
+/**
+ * @url: https://codepen.io/nathantaylor/pen/wrBQWd
+ */
 import React from "react"
 import styled, { keyframes, css } from "styled-components"
-import { adjustHue } from "polished"
 
-const metric = "s";
-const time = "1";
-const d = "0.08em"
+const metric = "s"
+const time = 1;
+const d = "0.08em";
 const n = 3;
-const e = "cubic-bezier(0.860, 0.000, 0.070, 1.000)";
-const front = "black"
-const back = "lightblue"
+const e = "cubic-bezier(0.860, 0.000, 0.070, 1.000)"
+const primaryColor = "#CE0058"
+const secondaryColor = "#000"
+const shadowColor = "lightblue"
+const backgroundColor = "#fff"
 
 const effect = keyframes`
-0%{
+0% {
   transform: translate3d(0,0,0);
-  text-shadow: 0em 0em 0 ${back};
-  color: ${front};
+  text-shadow: 0em 0em 0 ${shadowColor};
+  color: ${primaryColor};
 }
-30%{
+
+30% {
   transform: translate3d(0,0,0);
-  text-shadow: 0em 0em 0 ${back};
-  color: ${front};
+  text-shadow: 0em 0em 0 ${shadowColor};
+  color: ${secondaryColor};
 }
-70%{
+
+70% {
   transform: translate3d(${d},-${d},0);
-  text-shadow: -${d} ${d} ${back};
-  color: ${front};
+  text-shadow: -${d} ${d} ${shadowColor};
+  color: ${primaryColor};
 }
-100%{
+
+100% {
   transform: translate3d(${d},-${d},0);
-  text-shadow: -${d} ${d} ${back};
-  color: ${front};
+  text-shadow: -${d} ${d} ${shadowColor};
+  color: ${secondaryColor};
 }
-`;
+`
 
 const Container = styled.div`
-  background: white;
-`;
+  background: ${backgroundColor};
+`
 
 const Paragraph = styled.h1`
   font-family: Futura, sans-serif;
   font-weight: 900;
-  font-size: 80px;
-  padding: 80px;
-`;
+  font-size: 5rem;
+  padding: 5rem;
+
+  @media (max-width: 449.999999px) {
+    font-size: 2rem;
+    padding: 2rem;
+  }
+
+  @media (min-width: 450px) and (max-width: 767.999999px) {
+    font-size: 3rem;
+    padding: 3rem;
+  }
+
+  @media (min-width: 768px) and (max-width: 1169.999999px) {
+    font-size: 4rem;
+    padding: 4rem;
+  }
+
+  @media (min-width: 1170px) and (max-width: 1439.999999px) {
+    font-size: 5rem;
+    padding: 5rem;
+  }
+`
 
 const Letter = styled.span`
   position: relative;
   display: inline-block;
   animation: ${effect} ${time}s infinite alternate ${e};
 
-  ${[...Array(n).keys()].map((i) => 
-    css`
-      &:nth-last-child(${i + 1}n) {
-        animation-delay: -${(time * i) / n / 2}${metric};
-      }
-    `
+  ${[...Array(n).keys()].map(
+    (i) =>
+      css`
+        &:nth-last-child(${i + 1}n) {
+          animation-delay: -${(time * (i + 1)) / n / 2}${metric};
+        }
+      `
   )}
-  /* @for $i from 1 through $n {
-    &:nth-last-child(#{$i}n) {
-      animation-delay: -$t * $i/$n/2;
-    }
-  } */
 `
 
 const Covercc9a = ({ text }) => (
   <Container>
     <Paragraph>
-      <Letter>U</Letter>
-      <Letter>N</Letter>
-      <Letter>A</Letter>
-      <Letter>B</Letter>
-      <Letter>R</Letter>
-      <Letter>E</Letter>
-      <Letter>V</Letter>
-      <Letter>E</Letter>
-      <br />
-      <Letter>H</Letter>
-      <Letter>I</Letter>
-      <Letter>S</Letter>
-      <Letter>T</Letter>
-      <Letter>O</Letter>
-      <Letter>R</Letter>
-      <Letter>I</Letter>
-      <Letter>A</Letter>
-      <br />
-      <Letter>D</Letter>
-      <Letter>E</Letter>
-      <Letter>D</Letter>
-      <Letter>E</Letter>
-      <Letter>B</Letter>
-      <Letter>I</Letter>
-      <Letter>A</Letter>
-      <Letter>N</Letter>
+      {text
+        .split("")
+        .map((letter) => (letter === " " ? <br /> : <Letter>{letter}</Letter>))
+        // .map((letter) => (<Letter>{letter}</Letter>))
+        }
     </Paragraph>
   </Container>
 )
