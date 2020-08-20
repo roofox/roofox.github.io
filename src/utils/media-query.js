44ac1117642b524e2d165d2a38621c792d286b50
Sergio Flores <byoigres@gmail.com>
2020-08-20 11:20:03 -0600

    Added styled-media-query and created user-defined media

---
+++ src/utils/media-query.js
@@ -0,0 +1,11 @@
+import { generateMedia } from "styled-media-query"
+
+const media = generateMedia({
+  xs: "249.999999px",
+  sm: "449.999999px",
+  md: "767.999999px",
+  lg: "1169.999999px",
+  xl: "1439.999999px",
+})
+
+export default media
